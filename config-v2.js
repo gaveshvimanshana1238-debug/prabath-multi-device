ca78da6cb25653df93c23fa6199d6849e6ee90cb
gaveshvimanshana1238-debug <gaveshvimanshana1238@gmail.com>
2026-02-21 06:52:35 +0530

    Update config-v2.js

--- config-v2.js
+++ config-v2.js
@@ -1,4 +1,4 @@
-const DATABASE = "mongodb+srv://gaveshvimanshna722_db_user:<db_password>@cluster0.5gbtdgw.mongodb.net/?appName=Cluster0" // මෙතනට mongodb,mysql,postgresql or github token එකක් enter කරන්න මේ "" දෙක මැදට. (Enter a mongodb,mysql,postgresql or github token here between these two "".)
+const DATABASE = "mongodb+srv://gaveshvimanshna722_db_user:dbvimanshnagavesh88@cluster0.5gbtdgw.mongodb.net/?appName=Cluster0" // මෙතනට mongodb,mysql,postgresql or github token එකක් enter කරන්න මේ "" දෙක මැදට. (Enter a mongodb,mysql,postgresql or github token here between these two "".)
 const BOT_NUMBER = "94742549935" // ඔයාගෙ whatsapp number එක. (your whatsapp number)
 const SESSION_ID = "PRABATH-MD~SVtygvSU2nC571t" // ඔයාගෙ session id එක මේකෙන් ගන්න - https://prabathmd-official.vercel.app
 
